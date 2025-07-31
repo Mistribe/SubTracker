43c51a81424124da4268fd761f8cac7e7d6ca366
Oleexo <maxime.charles1@proton.me>
2025-07-31 23:00:41 +0200

    Update providers API query parameters to `offset` and `limit`, replacing `page` and `size`. Adjust request builders and serializers accordingly.

--- mobile/lib/api/providers/providers_request_builder_get_query_parameters.dart
+++ mobile/lib/api/providers/providers_request_builder_get_query_parameters.dart
@@ -4,16 +4,16 @@ import 'package:microsoft_kiota_abstractions/microsoft_kiota_abstractions.dart';
 /// auto generated
 /// Retrieve a paginated list of all providers with their plans and prices
 class ProvidersRequestBuilderGetQueryParameters implements AbstractQueryParameters {
-    ///  Page number (default: 1)
-    int? page;
-    ///  Items per page (default: 10)
-    int? size;
+    ///  Limit per request (default: 10)
+    int? limit;
+    ///  Offset (default: 0)
+    int? offset;
     /// Extracts the query parameters into a map for the URI template parsing.
     @override
     Map<String, dynamic> toMap() {
         return {
-            'page' : page,
-            'size' : size,
+            'limit' : limit,
+            'offset' : offset,
         };
     }
 }
