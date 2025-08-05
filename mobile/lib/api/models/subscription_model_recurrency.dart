6b56477b455329c31cd1b381593a1a150091d182
Oleexo <maxime.charles1@proton.me>
2025-08-05 23:31:41 +0200

    Expand `SubscriptionModelRecurrency` with new options: "unknown," "one_time," and "half_yearly." Update Kiota lock file.

--- mobile/lib/api/models/subscription_model_recurrency.dart
+++ mobile/lib/api/models/subscription_model_recurrency.dart
@@ -2,10 +2,11 @@
 /// auto generated
 ///  @Description Billing recurrency pattern (monthly, yearly, custom, etc.)
 enum SubscriptionModelRecurrency {
-    daily('daily'),
-    weekly('weekly'),
+    unknown('unknown'),
+    oneTime('one_time'),
     monthly('monthly'),
     quarterly('quarterly'),
+    halfYearly('half_yearly'),
     yearly('yearly'),
     custom('custom');
     const SubscriptionModelRecurrency(this.value);
