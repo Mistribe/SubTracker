887dc6267122b4a6a2006c47d2306093a165c24d
Oleexo <maxime.charles1@proton.me>
2025-08-08 23:24:58 +0200

    Add account deletion logic with error handling in `UsersRequestBuilder`
    - Added `deleteAsync` and `toDeleteRequestInformation` methods for deleting authenticated user accounts.
    - Incorporated error handling for 401 and 500 responses using `HttpError`.
    - Updated imports and configuration in `kiota-lock.json`.

--- mobile/lib/api/users/users_request_builder.dart
+++ mobile/lib/api/users/users_request_builder.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: type=lint
 import 'package:microsoft_kiota_abstractions/microsoft_kiota_abstractions.dart';
+import '../models/http_error.dart';
 import './preferred/preferred_request_builder.dart';
 import './profile/profile_request_builder.dart';
 
@@ -27,4 +28,22 @@ class UsersRequestBuilder extends BaseRequestBuilder<UsersRequestBuilder> {
     ///  [rawUrl] The raw URL to use for the request builder.
     ///  [requestAdapter] The request adapter to use to execute the requests.
     UsersRequestBuilder.withUrl(String rawUrl, RequestAdapter requestAdapter) : super(requestAdapter, "{+baseurl}/users", {RequestInformation.rawUrlKey : rawUrl}) ;
+    /// Deletes the authenticated user's account
+    ///  [requestConfiguration] Configuration for the request such as headers, query parameters, and middleware options.
+    Future<void> deleteAsync([void Function(RequestConfiguration<DefaultQueryParameters>)? requestConfiguration]) async {
+        var requestInfo = toDeleteRequestInformation(requestConfiguration);
+        final errorMapping = <String, ParsableFactory<Parsable>>{
+            '401' :  HttpError.createFromDiscriminatorValue,
+            '500' :  HttpError.createFromDiscriminatorValue,
+        };
+        return await requestAdapter.sendNoContent(requestInfo, errorMapping);
+    }
+    /// Deletes the authenticated user's account
+    ///  [requestConfiguration] Configuration for the request such as headers, query parameters, and middleware options.
+    RequestInformation toDeleteRequestInformation([void Function(RequestConfiguration<DefaultQueryParameters>)? requestConfiguration]) {
+        var requestInfo = RequestInformation(httpMethod : HttpMethod.delete, urlTemplate : urlTemplate, pathParameters :  pathParameters);
+        requestInfo.configure<DefaultQueryParameters>(requestConfiguration, () => DefaultQueryParameters());
+        requestInfo.headers.put('Accept', 'application/json');
+        return requestInfo;
+    }
 }
