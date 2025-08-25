8ed4fffa0c338f1c7e76c4c04d1ea0c62befdca3
Oleexo <maxime.charles1@proton.me>
2025-08-25 19:11:08 +0200

    Update import path in validation tests from `oleexo` to `mistribe`

--- backend/pkg/x/validation/vaidation_test.go
+++ backend/pkg/x/validation/vaidation_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/oleexo/subtracker/pkg/x/validation"
+	"github.com/mistribe/subtracker/pkg/x/validation"
 )
 
 func TestNewError(t *testing.T) {
